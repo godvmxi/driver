652bb14db0dc19572f25db0a455e2f2e9a99298e
root <root@ubuntu.(none)>
2016-02-29 12:47:51 +0000

    save

--- hello/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/infotm/kernel/hello/hello.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.13.0-32-generic/scripts/module-common.lds --build-id  -o /home/infotm/kernel/hello/hello.ko /home/infotm/kernel/hello/hello.o /home/infotm/kernel/hello/hello.mod.o
--- hello/.tmp_versions/hello.mod
+++
@@ -1,2 +0,0 @@
-/home/infotm/kernel/hello/hello.ko
-/home/infotm/kernel/hello/hello.o
